9012dd519d9de6204688b9502bcc22df35008e21
Kiu Man Yeung <118100055@umail.ucc.ie>
2021-02-18 05:09:21 +0000

    Added user's post to profile. Reduce duplicated html

--- CS3305TSP/webpages/models.py
+++ CS3305TSP/webpages/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     county = models.CharField(max_length=128, default='')
 
     date_posted = models.DateField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User,related_name="user_posts", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
