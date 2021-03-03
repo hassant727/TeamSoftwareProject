2b841013aa0a07ae32743d2cabfb00d8498d3c07
Kieran O'Sullivan <78323642+118388951@users.noreply.github.com>
2021-03-03 20:02:13 +0000

    Delete CS3305TSP/userdata directory

--- CS3305TSP/userdata/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.views import View
-from .views import UserdatFucntionView
-urlpatterns = [
-    path('userdata/', UserdatFucntionView.as_view(), name='userdata'),
-
-]
