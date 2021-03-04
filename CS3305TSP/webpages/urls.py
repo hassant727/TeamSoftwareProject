b095db1da4ac9cedc7d796937c6b0789e0c8d2f0
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-04 01:19:54 +0000

    added the path for the search bar

--- CS3305TSP/webpages/urls.py
+++ CS3305TSP/webpages/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
@@ -7,6 +9,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='home-page'),
     path('about/', views.aboutfunction, name='about-page'),
     path('userguide/', views.howToUse, name='userguide'),
+    path('search_posts', csrf_exempt(views.search_posts), name='search_posts'),
     path('search/', views.searchfunction, name='search-page'),
     path('post/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
