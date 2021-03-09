a4c74daf8502be17f52fbecd3f719ced1ac4c28b
kevinmukuna <47334692+kevinmukuna@users.noreply.github.com>
2021-03-09 00:03:56 +0000

    update the admin url from admin to tsp13

--- CS3305TSP/CS3305TSP/urls.py
+++ CS3305TSP/CS3305TSP/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # path('register/', views.register, name='register'),
-    path('admin/', admin.site.urls),
+    path('tsp13/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='userpages/login.html'), name='login-page'),
     path('logout/', auth_views.LogoutView.as_view(template_name='userpages/logout.html'), name='logout-page'),
     path('', include('userpages.urls')),
