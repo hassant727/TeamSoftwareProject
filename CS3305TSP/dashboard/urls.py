c2e6132223b15de9e27aba49ff86c786ebf721e5
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-10 16:42:02 +0000

    added the all_function url, this is used for dashboard manager

--- CS3305TSP/dashboard/urls.py
+++ CS3305TSP/dashboard/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard', views.dashboardfunction),
+    path('dashboard', views.dashboardfunction, name="dashboard"),
+    path('dashboard/function', views.dashboard_user_functionality, name="function"),
+
 ]
