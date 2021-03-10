bbeb6ca9c98d098d84b4438875ff97345928deb1
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-10 16:42:41 +0000

    added the all_function view, this is used for dashboard manager

--- CS3305TSP/dashboard/views.py
+++ CS3305TSP/dashboard/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+@login_required
 def dashboardfunction(request):
     return render(request, 'dashboard/dashboard.html')
+
+@login_required
+def dashboard_user_functionality(request):
+    return render(request, 'dashboard/all_user_function.html')
 
