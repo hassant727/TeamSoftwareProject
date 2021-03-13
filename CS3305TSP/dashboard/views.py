891cc2f8a617fe8ae737f5131489615de8a51bba
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-13 07:00:45 +0000

    moved the dashboard view to webpage views

--- CS3305TSP/dashboard/views.py
+++ CS3305TSP/dashboard/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+# @login_required
+# def dashboardfunction(request):
+#     return render(request, 'dashboard/dashboard.html')
+#
+# @login_required
+# def dashboard_user_functionality(request):
+#     return render(request, 'dashboard/all_user_function.html')
 
-@login_required
-def dashboardfunction(request):
-    return render(request, 'dashboard/dashboard.html')
-
-@login_required
-def dashboard_user_functionality(request):
-    return render(request, 'dashboard/all_user_function.html')
-
+# @login_required
+# def listing_total(request, **kwargs):
+#     # user = get_object_or_404(User, username=kwargs.get('username'))
+#     post = CS3305TSP.Post.objects.all()
+#     total_invoice = post.aggregate(total=Sum('estimated_price'))['total']
+#     # print(total_invoice)
+#     return render(request, 'dashboard/dashboard.html', {'total_invoice': total_invoice})
