543e037438ac88941950253c4a2513e0f0faced4
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-13 01:51:20 +0000

    added form fields for user first and last name in both create and update form

--- CS3305TSP/userpages/forms.py
+++ CS3305TSP/userpages/forms.py
@@ -5,20 +5,29 @@ from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    # email = forms.EmailField()
+    username = forms.CharField()
+    first_name = forms.CharField(max_length=32, help_text='First name')
+    last_name = forms.CharField(max_length=32, help_text='Last name')
+    email = forms.EmailField(max_length=64, help_text='Enter a valid email address')
+    password1 = forms.CharField()
+    password2 = forms.CharField()
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 
 # forms to update the email and users name
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    username = forms.CharField()
+    first_name = forms.CharField(max_length=32, help_text='First name')
+    last_name = forms.CharField(max_length=32, help_text='Last name')
+    email = forms.EmailField(max_length=64, help_text='Enter a valid email address')
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'first_name', 'last_name']
 
 
 # forms to update the image
