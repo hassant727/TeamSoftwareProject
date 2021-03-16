b7dfabeba62450eccc4f8136a27193741f0881e2
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-16 23:20:28 +0000

    added documentation and comment

--- CS3305TSP/webpages/forms.py
+++ CS3305TSP/webpages/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Post
 
 
+"""
+    defines the model form named UserdataModelForm
+    the model creates a form of type post
+    it excludes headline
+    orders teh post in it ascending order
+"""
 class UserdataModelForm(forms.ModelForm):
     class Meta:
         model = Post
