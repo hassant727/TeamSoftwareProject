b5cf32832fe6ad4e06ddd86034b837cbae6b1f21
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-16 23:19:57 +0000

    added documentation and comment

--- CS3305TSP/webpages/admin.py
+++ CS3305TSP/webpages/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
+"""
+    registering the post section in the admin section
+"""
 admin.site.register(Post)
 
 
