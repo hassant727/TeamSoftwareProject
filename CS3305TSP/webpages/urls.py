c0471ebcf8178e929bd0b2aefbca3615ab405582
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-16 23:20:54 +0000

    added documentation and comment

--- CS3305TSP/webpages/urls.py
+++ CS3305TSP/webpages/urls.py
@@ -3,7 +3,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, SearchResultView
 from . import views
-
+"""
+    urls path are define below
+    name is used for template referencing
+    the first entry in the function path is the url/that_name
+    thos are imported from the views, which define are teh templates are rendered
+"""
 urlpatterns = [
     # path('', views.homefunction, name='home-page'),
     path('', PostListView.as_view(), name='home-page'),
