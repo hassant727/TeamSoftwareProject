7c2e44ac645db52d99d675eeb44654f0db4fef31
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-16 23:21:05 +0000

    added documentation and comment

--- CS3305TSP/webpages/models.py
+++ CS3305TSP/webpages/models.py
@@ -5,15 +5,12 @@ from django.urls import reverse
 from django.template.defaultfilters import slugify
 from .price_predictor import predict
 
-"""
-this code creates a migrations, takes in the class below and creates an sql 
-query
-"""
+
 
 """
-run the following code to see what sql query the migration will run python 
-manage.py sqlmigrate blog (folder name in our case blog) sequence number 
-created in migration python manage.py sqlmigrate blog 0001 --> for our case 
+    run the following code to see what sql query the migration will run python 
+    manage.py sqlmigrate name_of_app (folder name in our case blog) sequence number 
+    created in migration python manage.py sqlmigrate name_of_app 0001 --> for our case 
 """
 
 COUNTY_CHOICES = [
@@ -100,27 +97,34 @@ property_type_dict = {'Detached House': 1,
                       'Duplex': 4
                       }
 
-"""
-retuens the path that contains the address of the post for the image
-"""
+
 def get_image_filename(instance, filename):
+    """
+        returns the path that contains the address of the post for the image
+    """
     post = instance.post
     address = "-".join(item for item in [post.address_line_1, post.address_line_2, post.city, post.county] if item)
     slug = slugify(address)
     return "post_images/%s-%s" % (slug, filename)
 
 
-"""
-the below takes in all the parameter needed for the price prediction and 
-returns the estimated price
-"""
+
 def price_pridiction_model(number_of_bedrooms, number_of_bathrooms, size, property_type, energy_rating):
+    """
+        the below takes in all the parameter needed for the price prediction and
+        returns the estimated price
+    """
     attributes = [[number_of_bedrooms, number_of_bathrooms, size, property_type, energy_rating]]
     estimated_price = predict(attributes)
     return estimated_price
 
 
 class Post(models.Model):
+    """
+        this code creates a migrations, takes in the class below and creates an sql
+        query
+        the fields are defined below come teh model defined above
+    """
     property_description = models.TextField(null=True, blank=True)
     address_line_1 = models.CharField(max_length=255)
     address_line_2 = models.CharField(max_length=255, null=True, blank=True)
@@ -140,10 +144,10 @@ class Post(models.Model):
     estimated_price = models.PositiveIntegerField(null=False, blank=True, editable=False)
     __original_price = None
 
-    """
-    returns the address of a post
-    """
     def __str__(self):
+        """
+            returns the address of a post
+        """
         address = ""
         address_list = [self.address_line_1, self.address_line_2, self.city, self.county]
         for i in range(4):
@@ -153,23 +157,22 @@ class Post(models.Model):
                     address = address + ", "
         return address
 
-    """
-        this method return a string to redirect the user after posting to that 
-        post by returning post details the primary key of the newly created 
-        post
-    """
     def get_absolute_url(self):
         """
-        the pk is used to identify each post, meaning when each post is creted, 
-        it's assigned a pk number :return:
+            this method return a string to redirect the user after posting to that
+            post by returning post details the primary key of the newly created
+            post
+
+            the pk is used to identify each post, meaning when each post is creted,
+            it's assigned a pk number :return:
         """
         return reverse('post-detail', kwargs={'pk': self.pk})
 
-    """
-        overwriting the django save method 
-        by passing in the estimated price into the price before it is saved
-    """
     def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        """
+            overwriting the django save method
+            by passing in the estimated price into the price before it is saved
+        """
         estimated_price = price_pridiction_model(
             self.number_of_bedrooms,
             self.number_of_bathrooms,
@@ -182,6 +185,9 @@ class Post(models.Model):
 
 
 class PostImage(models.Model):
+    """
+        the model is useed for image posting and image deletion
+    """
     image = models.ImageField('images', upload_to=get_image_filename, null=True, blank=True)
     post = models.ForeignKey(Post, related_name='post_images', on_delete=models.CASCADE)
 
