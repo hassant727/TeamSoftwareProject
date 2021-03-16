764506d60710e9aab4b8cf10db91e8f88f0efcda
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-16 23:20:13 +0000

    added documentation and comment

--- CS3305TSP/webpages/apps.py
+++ CS3305TSP/webpages/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
+"""
+    this is created when you create a new app ( python manage.py startapp name_of_app)
+    this represent an app your a main aplication
+    remember that in django we can have multiple apps in an app
+"""
+
 
 class WebpagesConfig(AppConfig):
     name = 'webpages'
