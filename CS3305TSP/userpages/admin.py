b4fcc60d6ceb525f0eded6fad79351775f852b45
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-20 03:29:10 +0000

    added the documentation and explanation

--- CS3305TSP/userpages/admin.py
+++ CS3305TSP/userpages/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 
+"""
+    registering the Profile section in the admin section
+    this means gives you access to viewing all users from the admin portal
+"""
 admin.site.register(Profile)
