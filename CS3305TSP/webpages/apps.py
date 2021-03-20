f4b1edb3090cd5c1d289026425cf3918cf3a1aa3
kevinmukuna <mukuna.kevin@gmail.com>
2021-03-20 03:34:00 +0000

    added the documentation and explanation

--- CS3305TSP/webpages/apps.py
+++ CS3305TSP/webpages/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 """
     this is created when you create a new app ( python manage.py startapp name_of_app)
-    this represent an app your a main aplication
+    this represent an app your a main application
     remember that in django we can have multiple apps in an app
 """
 
